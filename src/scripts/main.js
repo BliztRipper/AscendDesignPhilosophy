617b853d71c4ca2485fdfa2d301672d4c447aef0
Arnon <nonthegenocide@me.com>
2018-05-07 18:23:21 +0700

    add animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
 //@prepros-prepend ../../node_modules/aos/dist/aos.js
 
+AOS.init();
+
 var acc = document.getElementsByClassName("accordion");
 var i;
-
+ 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
