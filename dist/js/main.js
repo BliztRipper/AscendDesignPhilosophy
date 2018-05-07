b80ccdb67ff802288250d05f60f0082cc7c6115d
Arnon <nonthegenocide@me.com>
2018-05-07 18:12:44 +0700

    finish hompage

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+var i,acc=document.getElementsByClassName("accordion");for(i=0;i<acc.length;i++)acc[i].addEventListener("click",function(){this.classList.toggle("active");var panel=this.nextElementSibling;panel.style.maxHeight?panel.style.maxHeight=null:panel.style.maxHeight=panel.scrollHeight+"px"});
