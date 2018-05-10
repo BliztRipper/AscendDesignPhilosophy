0243f10aa21be901ddf15852d6ee71abdf40f81b
Arnon <nonthegenocide@me.com>
2018-05-10 16:32:05 +0700

    make parallax bh

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,3 @@
 //@prepros-prepend ../../node_modules/aos/dist/aos.js
 //@prepros-prepend ../../node_modules/jquery/dist/jquery.min.js
-
-
 AOS.init();
-
-$('#viewmore-home').click(function () {
-  $('html,body').animate({
-      scrollTop: $('.card.h-screen.bg-grey-light').offset().top
-    },
-    'slow');
-});
-
-var acc = document.getElementsByClassName("accordion");
-var i;
- 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
